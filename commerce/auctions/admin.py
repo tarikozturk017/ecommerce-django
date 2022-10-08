112577d8c7a71b4149ee3c0f37cea3f9adb49ca6
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-08 10:55:06 -0400

    Admin.py modified & models created

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+# TO DO -> list displays 
 
 # Register your models here.
+admin.site(Listing)
+admin.site(User)
+admin.site(Bid)
+admin.site(Comment)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,24 @@
+from pydoc import describe
+from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    current_price = models.DecimalField(max_digits=100, decimal_places=2)
+    url = models.URLField(blank=True) #optionally add url field
+    category = models.CharField(blank = True) #optionally add category field
+    active = models.BooleanField(default = True) #if the listing is active, default active
+
 class User(AbstractUser):
+    watch_list = models.ManyToManyField(Listing, blank = True, related_name = "list_items")
+
+class Bid(models.Model):
+    bid = models.DecimalField(max_digits=100, decimal_places=2)
+
+class Comment(models.Model):
     pass
