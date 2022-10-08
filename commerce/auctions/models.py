f977c7d73753911bdab247c67e09e0549153857c
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-10-08 12:21:18 -0400

    models are updated

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,6 +3,14 @@ from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .models import *
+
+class Comment(models.Model):
+    comment_title = models.CharField(max_length=64)
+    content = models.CharField(max_length = 256)
+
+class Bid(models.Model):
+    bid = models.DecimalField(max_digits=100, decimal_places=2)
 
 
 class Listing(models.Model):
@@ -13,12 +21,10 @@ class Listing(models.Model):
     url = models.URLField(blank=True) #optionally add url field
     category = models.CharField(blank = True) #optionally add category field
     active = models.BooleanField(default = True) #if the listing is active, default active
+    comments = models.ManyToManyField(Comment, blank = True, related_name="listings")
+    bids = models.ManyToManyField(Bid, related_name="listings")
 
 class User(AbstractUser):
-    watch_list = models.ManyToManyField(Listing, blank = True, related_name = "list_items")
-
-class Bid(models.Model):
-    bid = models.DecimalField(max_digits=100, decimal_places=2)
-
-class Comment(models.Model):
-    pass
+    watch_list = models.ManyToManyField(Listing, blank = True, related_name = "user")
+    bids = models.ManyToManyField(Bid, blank=True, related_name="user")
+    comments = models.ManyToManyField(Comment, blank=True, related_name="user")
